62963313b34825ffc171aa34a86e6d6e6de6d366
Dan <danre4@gmail.com>
2021-07-31 15:31:18 +0200

    testing public/

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,12 @@ module.exports = {
     mode: "development",
     entry: "./src/App.fs.js",
     output: {
-        path: path.join(__dirname, "./docs"),
+        path: path.join(__dirname, "./public"),
         filename: "bundle.js",
     },
     devServer: {
         publicPath: "/",
-        contentBase: "./docs",
+        contentBase: "./public",
         port: 8080,
     },
     module: {
